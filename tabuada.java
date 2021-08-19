0ccbd0fd57347ab3bc64cc9f9da4ec9ed20c0087
Yago sales <55598751+Yago-sales@users.noreply.github.com>
2021-08-19 17:43:44 -0300

    Add files via upload

---
+++ tabuada.java
@@ -0,0 +1,12 @@
+/*Nome: Yago Sales
+Matricula: 17244*/
+
+public class tabuada {
+  public static void main(String[] args) {
+
+    for (int i = 0; i <= 10; i++) {
+      System.out.println("10 * " + i + " = " + (10 * i));
+    }
+
+  }
+}
