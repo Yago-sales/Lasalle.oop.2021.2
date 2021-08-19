3501585c6e1f4de5048a699c22aa94c097ef6dc3
Yago Sales <yagosalleslol@gmail.com>
2021-08-19 20:10:18 -0300

    Programa ler nome

---
+++ LerNome.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LerNome {
+
+    public static void main(String[] args) {
+
+        /*
+         * String nome = ""; int idade = 0; Scanner teclado = new Scanner(System.in);
+         * 
+         * System.out.println("Digite um nome:"); nome = teclado.next();
+         * 
+         * System.out.println("Digite sua idade:"); // idade =
+         * Integer.parseInt(teclado.next()); idade = teclado.nextInt();
+         * System.out.println("Olá " + nome + ", boa noite !");
+         * System.out.printf("Você tem %d anos de idade", idade);
+         * 
+         * teclado.close();
+         */
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(1 + i);
+        }
+
+    }
+
+}
